ae69bf49e78c5144c49c79eec3c7e0b794937d74
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-05-09 18:03:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 
 # soup 은 데이터 추츨하는 역할
 
-indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
+indeed_result = requests.get("https://indeed.com/jobs?q=python&limit=50&start=99999")
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", class_="pagination")
 
-pages = pagination.find_all("a")
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
-print(spans[:-1])  # 마지막 <span class = pn> item을 제외시킴
+links = pagination.find_all("a")
+pages = []
+for link in links[:-1]:
+    pages.append(link.string)
+print(pages[-1])  # 마지막 <span class = pn> item을 제외시킴
